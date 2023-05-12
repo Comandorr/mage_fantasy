136ca292653997c67925f50fb0d4b781628e66e8
Comandorr <Plane135790@gmail.com>
2023-05-12 07:25:53 +0300

    run and idle animations

--- functions.py
+++ functions.py
@@ -1,6 +1,23 @@
-def load_animations(load):
+import arcade
+
+def load_animations(load, file):
     animations = {
-        'right':[],
-        'left':[]
-    }
-    
+        'right':{},
+        'left':{}
+        }
+    for y in range(len(load)):
+        anim = load[y][0]
+        count = load[y][1]
+        animations['right'][anim] = []
+        animations['left'][anim] = []
+        for x in range(0, count*32, 32):
+            t_r = arcade.load_texture(file, x, y*32, 32, 32)
+            t_l = arcade.load_texture(file, x, y*32, 32, 32, True)
+            id_r = sum(list(file.encode('utf8')))+x*100+y*10
+            id_l = sum(list(file.encode('utf8')))+x*100+y*10 + 1
+            duration = 100
+            if anim == 'run':
+                duration = 75
+            animations['right'][anim].append(arcade.AnimationKeyframe(id_r, duration, t_r))
+            animations['left'][anim].append(arcade.AnimationKeyframe(id_l, duration, t_l))
+    return animations
--- player_class.py
+++ player_class.py
@@ -1,12 +1,19 @@
 import arcade
 from pyglet.math import Vec2
+from functions import load_animations
 
 class Player(arcade.Sprite):
     def __init__(self, window, position):
         self.window = window
         self.scene = self.window.scene
+        self.frames = []
+        self.time_counter = 0
+        self.cur_frame_idx = 0
+        self.state = 'idle'
+        self.side = 'right'
+        file = 'resources/images/tilesets/player.png'
         super().__init__(
-            filename = 'resources/images/tilesets/player.png',
+            filename = file,
             image_x = 0,
             image_y = 32,
             image_width = 32,
@@ -17,35 +24,61 @@ class Player(arcade.Sprite):
             )
         
         self.speed = 5
-        self.load = {
-            'spawn':4, 
-            'idle':4, 
-            'run':8, 
-            'jump_idle':1, 
-            'jump_run':1, 
-            'land':3, 
-            'roll':8, 
-            'turn':4, 
-            'hit':4, 
-            'death':4
-            }
-
+        load = [
+            ['spawn',4], ['idle',4], ['run',8],  ['jump_idle',1], ['jump_run',1], 
+            ['land',3],  ['roll',8], ['turn',4], ['hit',4],       ['death',4], ]
+        self.animations = load_animations(load, file)
+        self.change_animation(self.side, self.state)
 
     def update(self, delta_time: float = 1 / 60):
         self.change_x = 0
         if self.window.keys[arcade.key.D] and not self.window.keys[arcade.key.A]:
             self.change_x = self.speed
+            if self.side != 'right':
+                self.change_animation('right', 'run')
         elif self.window.keys[arcade.key.A] and not self.window.keys[arcade.key.D]:
             self.change_x = -self.speed
+            if self.side != 'left':
+                self.change_animation('left', 'run')
 
         self.change_y = 0
         if self.window.keys[arcade.key.W] and not self.window.keys[arcade.key.S]:
             self.change_y = self.speed
+            if self.state != 'run':
+                self.change_animation(self.side, 'run')
         elif self.window.keys[arcade.key.S] and not self.window.keys[arcade.key.W]:
             self.change_y = -self.speed
+            if self.state != 'run':
+                self.change_animation(self.side, 'run')
+
+        if self.change_x == 0 and self.change_y == 0:
+            if self.state == 'run':
+                self.change_animation(self.side, 'idle')
+        else:
+            if self.state != 'run':
+                self.change_animation(self.side, 'run')
+            
+        
 
         vec = Vec2(self.change_x, self.change_y).normalize()
         self.change_x = vec[0]*self.speed
         self.change_y = vec[1]*self.speed
         super().update()
+        self.update_animation()
 
+    def change_animation(self, side, state):
+        self.side, self.state = side, state
+        self.frames = self.animations[self.side][self.state]
+        self.cur_frame_idx = -1
+        self.time_counter = 0
+        self.update_animation()
+
+    def update_animation(self, delta_time: float = 1 / 60):
+        self.time_counter += delta_time
+        while self.time_counter > self.frames[self.cur_frame_idx].duration / 1000.0:
+            self.time_counter -= self.frames[self.cur_frame_idx].duration / 1000.0
+            self.cur_frame_idx += 1
+            if self.cur_frame_idx >= len(self.frames):
+                self.cur_frame_idx = 0
+            cur_frame = self.frames[self.cur_frame_idx]
+            self.texture = cur_frame.texture
